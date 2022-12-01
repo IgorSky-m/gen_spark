b8b9dac2ccf4b5f8f4f05aab26acbef6c9bd20ee
Skachko Igar <scarydisco@gmail.com>
2022-12-01 10:18:36 -0600

    upd lifecycle example add Disposable bean interface and add prototype scope

--- week_3_assignment_1/src/main/java/edu/genspark/examples/lifecycle/LifecycleExampleMain.java
+++ week_3_assignment_1/src/main/java/edu/genspark/examples/lifecycle/LifecycleExampleMain.java
@@ -1,5 +1,6 @@
 package edu.genspark.examples.lifecycle;
 
+import edu.genspark.examples.lifecycle.entity.AnyObject;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -7,6 +8,9 @@ public class LifecycleExampleMain {
     public static void main(String[] args) {
         ConfigurableApplicationContext context =
                 new AnnotationConfigApplicationContext("edu.genspark.examples.lifecycle");
+
+        context.getBean(AnyObject.class);
+
         context.close();
     }
 }
--- week_3_assignment_1/src/main/java/edu/genspark/examples/lifecycle/entity/AnyObject.java
+++ week_3_assignment_1/src/main/java/edu/genspark/examples/lifecycle/entity/AnyObject.java
@@ -1,17 +1,52 @@
 package edu.genspark.examples.lifecycle.entity;
 
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
-public class AnyObject {
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+public class AnyObject implements BeanNameAware, DisposableBean {
 
-    private int i;
-
-    public int getI() {
-        return i;
+    public AnyObject(){
+        System.out.println("in AnyObject constructor");
     }
 
-    public void setI(int i) {
-        this.i = i;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @PostConstruct
+    private void postConstruct(){
+        System.out.println("in AnyObject Post Construct");
+    }
+
+    @PreDestroy
+    private void preDestroy(){
+        System.out.println("in AnyObject Pre Destroy");
+    }
+
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("in AnyObject bean name aware interface");
+        name = s + " Aware";
+    }
+
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("in AnyObject Disposable bean destroy");
     }
 }
